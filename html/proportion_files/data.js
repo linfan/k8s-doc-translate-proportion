430070e667adcb9997f1c3e836ea394d75707b70
Fan Lin <flin@thoughtworks.com>
2015-11-02 22:47:15 +0800

    separate title.html and data.js out

---
+++ html/proportion_files/data.js
@@ -0,0 +1,14 @@
+var json=[
+  {"name":"White", "value":"2994"},
+  {"name":"it2afl0rd", "value":"514"},
+  {"name":"kz", "value":"4"},
+  {"name":"mr.art", "value":"230"},
+  {"name":"razr", "value":"2185"},
+  {"name":"tiger", "value":"971"},
+  {"name":"李昂", "value":"25"},
+  {"name":"林帆", "value":"867"},
+  {"name":"王乐", "value":"1092"},
+  {"name":"卢文泉", "value":"1539"},
+  {"name":"安雪艳", "value":"1662"},
+  {"name":"李加庆", "value":"0"}
+]
