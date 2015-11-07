a371291ba808abf03a6e963aefc544b8505793b9
Fan Lin <flin@thoughtworks.com>
2015-11-07 23:39:22 +0800

    Update 2015-11-07_23:39 data

--- html/proportion_files/data.js
+++ html/proportion_files/data.js
@@ -1,16 +1,17 @@
 var json=[
-  {"name":"White", "value":"2994"},
+  {"name":"White", "value":"4961"},
   {"name":"it2afl0rd", "value":"1799"},
-  {"name":"kz", "value":"1543"},
+  {"name":"kz", "value":"4883"},
   {"name":"mr.art", "value":"230"},
   {"name":"razr", "value":"2441"},
   {"name":"tiger", "value":"971"},
+  {"name":"何炜", "value":"3"},
   {"name":"李昂", "value":"25"},
   {"name":"林帆", "value":"867"},
   {"name":"王乐", "value":"1092"},
   {"name":"卢文泉", "value":"1539"},
   {"name":"安雪艳", "value":"1662"},
-  {"name":"张以法", "value":"1551"},
+  {"name":"张以法", "value":"1569"},
   {"name":"李加庆", "value":"0"},
-  {"name":"钟健鑫", "value":"1124"}
+  {"name":"钟健鑫", "value":"1132"}
 ]
