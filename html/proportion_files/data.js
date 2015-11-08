141427bc64f43e41b4bf63295812715981b335c0
Fan Lin <flin@thoughtworks.com>
2015-11-09 00:01:27 +0800

    Update 2015-11-09_00:01 data

--- html/proportion_files/data.js
+++ html/proportion_files/data.js
@@ -1,17 +1,18 @@
 var json=[
   {"name":"White", "value":"5547"},
-  {"name":"it2afl0rd", "value":"1799"},
+  {"name":"it2afl0rd", "value":"2519"},
   {"name":"kz", "value":"4883"},
   {"name":"mr.art", "value":"230"},
   {"name":"razr", "value":"2441"},
   {"name":"tiger", "value":"971"},
-  {"name":"何炜", "value":"789"},
+  {"name":"何炜", "value":"1379"},
+  {"name":"张迪", "value":"18"},
   {"name":"李昂", "value":"25"},
-  {"name":"林帆", "value":"954"},
+  {"name":"林帆", "value":"3401"},
   {"name":"王乐", "value":"1092"},
   {"name":"卢文泉", "value":"1539"},
   {"name":"安雪艳", "value":"1662"},
   {"name":"张以法", "value":"1569"},
-  {"name":"李加庆", "value":"0"},
-  {"name":"钟健鑫", "value":"1154"}
+  {"name":"李加庆", "value":"2616"},
+  {"name":"钟健鑫", "value":"1190"}
 ]
