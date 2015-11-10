26e3996f468588222c80ccc0a3e6d60cb6153b22
Fan Lin <flin@thoughtworks.com>
2015-11-10 08:04:19 +0800

    Update 2015-11-10_08:04 data

--- html/proportion_files/data.js
+++ html/proportion_files/data.js
@@ -1,12 +1,13 @@
 var json=[
   {"name":"White", "value":"5547"},
   {"name":"it2afl0rd", "value":"2519"},
-  {"name":"kz", "value":"4883"},
-  {"name":"mr.art", "value":"230"},
+  {"name":"kz", "value":"5656"},
+  {"name":"mr.art", "value":"468"},
   {"name":"razr", "value":"2441"},
   {"name":"tiger", "value":"971"},
   {"name":"何炜", "value":"1379"},
   {"name":"张迪", "value":"18"},
+  {"name":"张鑫", "value":"217"},
   {"name":"李昂", "value":"25"},
   {"name":"林帆", "value":"3401"},
   {"name":"王乐", "value":"1092"},
@@ -14,5 +15,5 @@ var json=[
   {"name":"安雪艳", "value":"1662"},
   {"name":"张以法", "value":"1569"},
   {"name":"李加庆", "value":"2616"},
-  {"name":"钟健鑫", "value":"1190"}
+  {"name":"钟健鑫", "value":"1213"}
 ]
